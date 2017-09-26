cfdc542c5e5b3a1299b1a3620898c8836a00cb07
petersha <petersha@wirelessprv-10-194-1-195.near.illinois.edu>
2017-09-26 14:40:52 -0500

    All checkstyle errors fixed

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,4 +1,4 @@
-/*
+/**
  * The following code needs a lot of TLC. So give it some!
  *
  * 1. Fix all checkstyle errors
@@ -7,20 +7,33 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+public class ReclamationProject {
+    /**
+     * rearrange a and b, make the longer at the front.
+     * @param a input string
+     * @param b input string
+     * @return output string
+     */
+    static String doit(final String a, final String b) {
+       String tempA = a;
+       String tempB = b;
+        if (tempA.length() > tempB.length()) {
+            String c = tempA;
+            tempA = b;
+            tempB = c;
+            }
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    }
+                }
+            }
+        } return r;
+    }
 }
